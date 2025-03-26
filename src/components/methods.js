cb294c10bcbb9480c73765bd80f64f3950d5718e
nja-n <108585125+nja-n@users.noreply.github.com>
2025-03-27 02:24:57 +0530

    Update methods.js

--- src/components/methods.js
+++ src/components/methods.js
@@ -1,2 +1,2 @@
 export const game = "ws://maglev.proxy.rlwy.net:40170/game";
-export const saveUser = "api/cards/savePlayer";
+export const saveUser = "https://aetheracemaster-production.up.railway.app/cards/savePlayer";
