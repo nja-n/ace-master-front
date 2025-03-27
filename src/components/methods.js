180fc8f8ecf609c6822d55457ff3c23270301e2a
nja-n <108585125+nja-n@users.noreply.github.com>
2025-03-27 19:25:50 +0530

    Update methods.js

--- src/components/methods.js
+++ src/components/methods.js
@@ -1,2 +1,2 @@
-export const game = "ws://maglev.proxy.rlwy.net:40170/game";
+export const game = "wss://aetheracemaster-production.up.railway.app/game";
 export const saveUser = "https://aetheracemaster-production.up.railway.app/cards/savePlayer";
