1c149a8c219019e710800632cca40df1865e8005
nja-n <108585125+nja-n@users.noreply.github.com>
2025-04-07 19:42:17 +0530

    Update methods.js

--- src/components/methods.js
+++ src/components/methods.js
@@ -1,2 +1,3 @@
 export const game = "wss://aetheracemaster-production.up.railway.app/game";
 export const saveUser = "https://aetheracemaster-production.up.railway.app/cards/savePlayer";
+export const getTimeRemains = "http://localhost:8080/cards/SESSION_ID/remaining-time";
