730e69b980ee42de4d9a29f1387eb685bfac0a5d
vishnumg <vishnumg@kranadmin>
2025-05-02 02:33:32 +0530

    backend server changes

--- src/components/serverURL.js
+++ src/components/serverURL.js
@@ -1,3 +1,7 @@
-export const protocol = "http"; //https
-export const socket = "ws"; //wss
-export const server = "localhost:8080";
+// export const protocol = "http"; //https
+// export const socket = "ws"; //wss
+// export const server = "localhost:8080";
+
+export const protocol = "https"; //https
+export const socket = "wss"; //wss
+export const server = "aetheracemaster.onrender.com";
