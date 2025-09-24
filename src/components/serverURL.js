ffafbf921ceb005c16180ecdb103c4678bc520e1
VISHNU <v4vishnumg25@gmail.com>
2025-09-25 02:29:56 +0530

    testing updated

--- src/components/serverURL.js
+++ src/components/serverURL.js
@@ -5,5 +5,5 @@
 export const protocol = "https";
 export const socket = "wss";
 
-// export const server = "aetheracemaster.onrender.com/aeither";
-export const server = "ace-master.onrender.com/aeither";
+export const server = "aetheracemaster.onrender.com/aeither";
+// export const server = "ace-master.onrender.com/aeither";
